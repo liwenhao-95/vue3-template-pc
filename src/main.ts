27b152095b412b4464fba5e0517fff44183e3118
liwenhao <wenhao.li@bizfocus.cn>
2024-03-04 15:37:30 +0800

    首次添加

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 
 const app = createApp(App)
